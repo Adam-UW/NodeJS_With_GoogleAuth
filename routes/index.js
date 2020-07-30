892f426baefb258857129b113fece77697f4fd77
Adam Shandi <abbashasan91@gmail.com>
2020-07-30 14:15:19 -0700

    before add new stories implementation

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const router = express.Router()
 
 const {ensureAuth, ensureGuest} = require('../middleware/auth')
 
+const Story= require('../models/Story')
+
 // @desc Login/Landing page
 // @route GET /
 
@@ -16,11 +18,20 @@ router.get('/', ensureGuest, (req, res)=>{
 // @desc Dashboard
 // @route GET /dashboard
 
-router.get('/dashboard', ensureAuth, (req, res)=>{
-    // console.log(req.user)
+router.get('/dashboard', ensureAuth, async (req, res)=>{
+    try {
+        const stories= await Story.find({user: req.user.id}).lean()
+        // console.log(req.user)
     res.render('dashboard', {
-        name: req.user.firstName
+        name: req.user.firstName,
+        stories
     })
+        
+    } catch (error) {
+        console.log(err)   
+        res.render('error/500')  
+    }
+    
 })
 
 
